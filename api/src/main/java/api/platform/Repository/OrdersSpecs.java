c75ae03d4588a1e6c9a62e874fd807cfef66469b
zhangwenda4917 <3300491785@qq.com>
2020-11-27 22:57:43 +0800

    完成部分功能

---
+++ api/src/main/java/api/platform/Repository/OrdersSpecs.java
@@ -0,0 +1,47 @@
+package api.platform.Repository;
+
+import api.platform.Enyity.Orders;
+import api.platform.Enyity.Owner;
+import api.platform.Enyity.TheDriver;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.Expression;
+import java.sql.Driver;
+
+public class OrdersSpecs {
+    public static Specification<Orders> containingName(String startPlace) {
+        if (startPlace != null) {
+            return (Specification<Orders>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.like(root.get("startPlace").as(String.class), String.format("%%%s%%", startPlace));
+        } else {
+            return Specification.where(null);
+        }
+    }
+
+    public static Specification<Orders> startWithNo(String endPlace) {
+        if (endPlace == null) {
+            return Specification.where(null);
+        }
+        return (Specification<Orders>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.like(root.get("endPlace").as(String.class), String.format("%%%s%%", endPlace));
+    }
+
+    public static Specification<Orders> startWithOwner(Owner owner) {
+        if (owner == null) {
+            return Specification.where(null);
+        }
+        return (Specification<Orders>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.equal(root.get("owner").as(Owner.class),  owner);
+    }
+
+    public static Specification<Orders> startWithDriver(TheDriver theDriver) {
+        if (theDriver == null) {
+            return Specification.where(null);
+        }
+        return (Specification<Orders>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.equal(root.get("theDriver").as(TheDriver.class),  theDriver);
+    }
+
+    public static Specification<Orders> isStatus(Long status) {
+        if (status == null) {
+            return Specification.where(null);
+        }
+        return (Specification<Orders>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.equal(root.get("status").as(Long.class),  status);
+    }
+}
